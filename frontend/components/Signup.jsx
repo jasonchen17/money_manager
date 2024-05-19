47dac29dc328e8738104a8dd0d742c6513065ad9
Jason Chen <july8jason@gmail.com>
2024-05-18 22:27:30 -0400

    add signup styling

--- frontend/components/Signup.jsx
+++ frontend/components/Signup.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Axios from 'axios'
 import { Link, useNavigate } from 'react-router-dom';
+import styled from 'styled-components'
 
 const Signup = () => {
     const [username, setUsername] = useState("")
@@ -25,36 +26,107 @@ const Signup = () => {
     }
 
     return (
-        <div className='sign-up-container'>
-            <h2>Sign Up</h2>
-            <form className='sign-up-form' onSubmit={handleSubmit}>
-                <label htmlFor="username">Username:</label>
-                <input 
-                    type="text" 
-                    placeholder='Username' 
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-
-                <label htmlFor="email">Email:</label>
-                <input 
-                    type="email" 
-                    autoComplete='off' 
-                    placeholder='Email'
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-
-                <label htmlFor="password">Password:</label>
-                <input 
-                    type="password" 
-                    placeholder='******'
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-
-                <button type="submit">Sign Up</button>
-                <p>Have an Account? <Link to="/login">Login</Link></p>
+        <SignupContainer>
+            <form className='wrapper' onSubmit={handleSubmit}>
+                <h1>Sign Up</h1>
+                <div className='input-box'>
+                    <input
+                        type="text"
+                        placeholder='Username'
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                </div>
+                <div className='input-box'>
+                    <input
+                        type="email"
+                        autoComplete='off'
+                        placeholder='Email'
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div className='input-box'>
+                    <input
+                        type="password"
+                        placeholder='Password'
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <button type="submit" className="submit-button">Sign Up</button>
+                <div className="register">
+                    <p>Have an Account? <Link to="/login">Log in</Link></p>
+                </div>
             </form>
-        </div>
+        </SignupContainer>
     )
 }
+
+const SignupContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+
+    .wrapper {
+        width: 410px;
+        background: gray;
+        border-radius: 10px;
+        padding: 25px 35px;
+    }
+
+    .wrapper h1 {
+        font-size: 35px;
+        text-align: center;
+    }
+
+    .wrapper .input-box {
+        width: 100%;
+        height: 50px;
+        margin: 30px 0;
+    }
+
+    .input-box input {
+        width:100%;
+        height: 100%;
+        background: transparent;
+        border: none;
+        outline: none;
+        border: 2px solid black;
+        border-radius: 10px;
+        color: white;
+        padding: 20px 45px 20px 20px;
+    }
+
+    .input-box input::placeholder {
+        color: black;
+    }
+
+    .submit-button {
+        width: 100%;
+        height: 45px;
+        border: none;
+        outline: none;
+        background: white;
+        border-radius: 10px;
+        cursor: pointer;
+        font-size: 16px;
+        color: black;
+        font-weight: 600;
+    }
+
+    .register {
+        font-size: 14px;
+        text-align: center;
+        margin: 20px 0 10px;
+    }
+
+    .register p a {
+        text-decoration: none;
+        font-weight: 600;
+    }
+
+    .register p a:hover {
+        text-decoration: underline;
+    }
+`;
 
 export default Signup
